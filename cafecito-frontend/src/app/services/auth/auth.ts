5d4262974d2ef6b866a2357bbc607f0ff7fbdce3
Edson Alberto Moreno Ortega <ed_moreno@192.168.1.122>
2026-01-21 22:47:45 -0600

    refactor(frontend): configurar HttpClient y estandarizar nomenclatura de servicios
    1. Configuración Global (app.config.ts):
    - Se implementa provider 'provideHttpClient(withFetch())' para habilitar peticiones HTTP en la arquitectura Standalone.
    
    2. Servicios (Core):
    - Se añaden los archivos base de servicios en 'src/app/services'.
    - Se aplica convención de nombres de Angular: Las clases ahora usan el sufijo 'Service' (ej: Auth -> AuthService, Products -> ProductService) para mejorar la legibilidad y evitar conflictos de nombres con los modelos.

---
+++ cafecito-frontend/src/app/services/auth/auth.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  private apiUrl = 'http://localhost:3000/api/auth';
+
+  constructor(private http: HttpClient) {}
+
+  login(credentials: any): Observable<any> {
+    return this.http.post<any>(`${this.apiUrl}/login`, credentials);
+  }
+
+  register(userData: any): Observable<any> {
+    return this.http.post<any>(`${this.apiUrl}/register`, userData);
+  }
+}
