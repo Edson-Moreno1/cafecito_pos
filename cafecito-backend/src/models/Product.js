47a3ae768b49b6b2f3caa0dbdc5a46988d95ffa9
Edson Alberto Moreno Ortega <ed_moreno@192.168.1.54>
2026-02-19 21:58:21 -0600

    refactor: remove category and images from Product schema (out of MVP)

--- cafecito-backend/src/models/Product.js
+++ cafecito-backend/src/models/Product.js
@@ -1,12 +1,11 @@
 import mongoose from "mongoose";
 
 const productSchema = new mongoose.Schema({
-    
     name: {
         type: String,
         required: true,
-        maxlength: 100,
         minlength: 2,
+        maxlength: 100,
     },
     price: {
         type: Number,
@@ -23,23 +22,11 @@ const productSchema = new mongoose.Schema({
         type: String,
         required: false
     },
-    category: {
-        type: String,
-        required: true,
-        enum: {
-            values: ['Bebidas calientes','Frappucino','Bebidas frias','Bebidas Base Té','Cold Brew','Alimentos','Cafe en grano'],
-            message: '{VALUE} no es una categoriá válida'
-        }
-    },
-    images: [{
-        type: String
-    }],
     isActive:{
         type: Boolean,
         default: true
     }
-},
-{
+},{
     timestamps: true
 });
 
